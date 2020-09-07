32fc86c913d0c29e3f64d9a91f7d1339c163f40f
Rion-user <70883121+Rion-user@users.noreply.github.com>
2020-09-07 16:55:31 +0900

    machine-learning

---
+++ asdf2.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import pandas as pd
+
+파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
+보스턴 = pd.read_csv(파일경로)
+
+독립 = 보스턴[['crim', 'zn', 'indus', 'chas', 'nox',
+             'rm', 'age', 'dis', 'rad', 'tax',
+             'ptratio','b', 'lstat']]
+종속 = 보스턴['medv']
+
+X = tf.keras.layers.Input(shape=[13])
+H = tf.keras.layers.Dense(10, activation='swish')(X)
+H = tf.keras.layers.Dense(10, activation='swish')(H)
+Y = tf.keras.layers.Dense(1)(H)
+model = tf.keras.models.Model(X,Y)
+model.compile(loss = 'mse',optimizer = 'sgd', metrics = ['accuracy'])
+
+#model.fit(독립,종속,epochs = 10000, verbose = 0)
+model.fit(독립,종속,epochs = 10)
